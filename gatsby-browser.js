8daace61196081214996b8e834559161ec20003f
InnocentWahome <innocentwahome@gmail.com>
2022-04-25 14:03:58 +0300

    Initial Commit - Added Tailwind & Bulma

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,5 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/browser-apis/
- */
+import "./src/assets/css/global.css"
 
-// You can delete this file if you're not using it
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "./src/assets/scss/index.scss"
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Ekraus Website`,
+    description: `Official Website for Ekraus`,
+    author: `@InnocentWahome`,
+    siteUrl: `https://ekraus.netlify.app`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-sass`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
